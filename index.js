7b65fe79fc1e94b97b0adcc80e74cdc43b293384
Fuad9 <alfuad8548@gmail.com>
2020-12-11 09:43:01 +0600

    socket.io initialized

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const PORT = 5000;
-
 const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-   res.send("Hello from DB");
-   console.log("Hello for DB");
+   res.send(__dirname + "/index.html");
 });
 
-app.listen(PORT, () => console.log(`server is running at ${PORT}`));
+io.on("connection", (socket) => {
+   socket.on("chat message", (msg) => {
+      console.log("message: " + msg);
+   });
+});
+
+http.listen(PORT, () => console.log(`server is running at ${PORT}`));
